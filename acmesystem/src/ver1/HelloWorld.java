c8db75a319bc87b41151c38a426d1fd408b27e2c
Neema Rubadiri <neemarubadiri@gmail.com>
2026-09-02 02:16:47 -0400

    Implement whatFor method

--- acmesystem/src/ver1/HelloWorld.java
+++ acmesystem/src/ver1/HelloWorld.java
@@ -6,10 +6,15 @@ public class HelloWorld {
         System.out.println("Hello from Eclipse");
         System.out.println("Neema is working locally on her branch");
         heyNow();
+        whatFor("Neema");
     }
     
     public static void heyNow() {
         System.out.println("hey now");
+    }
+    
+    public static void whatFor(String stringArg) {
+        System.out.println(stringArg + ", what for?");
     }
 
 }
